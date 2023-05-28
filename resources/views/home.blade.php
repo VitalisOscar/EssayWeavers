00862c45d790fc934f652149d0e161fc9a0fb123
VitalisOscar <vitalisoscar25@gmail.com>
2023-05-28 22:39:11 +0300

    Homepage, google serach verification

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="ZdJ5b2sR2SV3GBeoIC67AASBaLjZBwX84UbZ2p3ayvw" />
 
-        <title>Laravel</title>
+        <title>EssayWeavers - Professional tutoring services</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
